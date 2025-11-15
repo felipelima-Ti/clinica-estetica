1a0bad1c36de25137909ab66c8b82f6fcd18673f
unknown <felipelima1114@gmail.com>
2025-11-15 20:11:37 -0300

    fix text in agenda

--- src/app/agenda/page.js
+++ src/app/agenda/page.js
@@ -15,7 +15,6 @@ export default function Agenda() {
   const [horariosDisponiveis, setHorariosDisponiveis] = useState([]);
   const router = useRouter();
 
-  // horários padrão (você pode ajustar os intervalos)
   const horariosBase = [
     "08:00", "09:00", "10:00", "11:00",
     "12:00", "13:00", "14:00", "15:00",
